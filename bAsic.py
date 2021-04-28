11af69b0dc873ad02de6aef34a230d934619df43
Jayanth <jayanthmj@users.noreply.github.com>
2021-04-28 12:06:38 +0530

    python basic learning

---
+++ bAsic.py
@@ -0,0 +1,22 @@
+# Basic python has all the new thing learned, it will be as regular as possible
+
+# printing in python is simple with print, and it works with both double"" and signal'' parantisis
+print("hello world!! \n") # \n provides newline, similarly \t for tab.
+print('Jayanth started learning python with this\n')
+
+# Types, I can see mainly 4 Types or datetypes -  int, float, str and bool
+int_a = 12
+float_a=1.2
+string_a="Jayanthkumar Mandya Jayaramu"
+
+print(string_a+" Nehru Nagar Mandya")
+print(int_a+12)
+print(22.1+float_a)
+
+#string can be played in many ways, it allows both + and -ve index.
+str="0123456789"
+print(str[0])
+print(str[1])
+print(str[-1])
+print(str[-10])
+print(str[9])
